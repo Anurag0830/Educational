7b543ed8f0b1d61544f78f42247327709481e1ce
Anurag <anuragpuri02@gmail.com>
2025-03-30 16:29:35 +0530

    fix-slider

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -10,13 +10,13 @@ import user_4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
      const slider = useRef();
-     let tx = 0;
+     let tx = -25;
 
     const slideForward=()=>{
-      if(tx >   -50){
+      if(tx > -50){
         tx -= 25;
       }
-      slider.current.style.transform='translateX(${tx}%)';
+      slider.current.style.transform=`translateX(${tx}%)`;
     }
     const slideBackward = ()=>{ 
       if(tx < 0){
